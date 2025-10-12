a067c29f4a68c64258b583959c11e80eaa7dcde3
SHINIGAMI <20.bogdanov.kirill.05@gmail.com>
2025-10-13 00:25:16 +0700

    version 2

--- Processing arrays.cpp
+++ Processing arrays.cpp
@@ -10,15 +10,13 @@ using namespace std;
 
     int main() {
 
-    const int m = 100, n = 100;
+    int m = 100, n = 100;
     double t[m][n];
     int a, b, c, d;
     
     cout << "Введите количество строк a и столбцов b: ";
     cin >> a >> b;
-    
     cout << "Введите элементы матрицы: " << endl;
-
     for (c = 0; c < a; ++c) {
         for (d = 0; d < b; ++d) {
             cin >> t[c][d];
@@ -26,7 +24,6 @@ using namespace std;
     }
     
     cout << "Исходная матрица:" << endl;
-
     for (c = 0; c < a; ++c) {
         for (d = 0; d < b; ++d) {
             cout << t[c][d] << " ";
@@ -34,22 +31,21 @@ using namespace std;
         cout << endl;
     }
     
-    cout << "\nРезультаты:" << endl;
+    cout << "Результаты:" << endl;
     cout << "Сумма отрицательных элементов | Количество отрицательных элементов" << endl;
-
+    double sum = 0;
+    int count = 0;
     for (c = 0; c < a; ++c) {
-        double sum = 0;
-        int count = 0;
+
         for (d = 0; d < b; ++d) {
             if (t[c][d] < 0) {
                 sum += t[c][d];
                 count++;
             }
         }
-        
+
         cout << sum << "   |   " << count << endl;
     }
     
     return 0;
 }
-
