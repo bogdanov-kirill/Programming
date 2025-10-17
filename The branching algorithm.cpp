e7027eee1e0f86a4bab15206eccfa1fc750afcc4
SHINIGAMI <20.bogdanov.kirill.05@gmail.com>
2025-10-18 01:40:11 +0700

    version 4

--- The branching algorithm.cpp
+++ The branching algorithm.cpp
@@ -11,45 +11,38 @@ using namespace std;
 
     int main() {
 
-    double D, h, C0, Cp, C;
-    int i_less;
-    double i_result;
-
-    cout << "i должно быть <= ";
-    cin >> i_less;
-    cout << "Введите D = ";
-    cin >> D;
-    cout << "Введите h = ";
-    cin >> h;
-    cout << "Введите C0 = ";
-    cin >> C0;
-    cout << "Введите Cp = ";
-    cin >> Cp;
-    cout << "Введите C = ";
-    cin >> C;
-
-    for (int i = 0; i <= i_less; ++i) {
-
-        double C;
-        switch (i) {
-            case 0: C = 8.0; break;
-            case 1: C = 7.5; break;
-            case 2: C = 7.0; break;
-            case 3: C = 6.0; break;
-            case 4: C = 4.0; break;
-            case 5: C = 2.0; break;
+        double D, h, C0, Cp, Cstart, Cend, step = 0.5, iResult;
+        
+        cout << "Enter the diffusion coefficient (D): ";
+        cin >> D;
+        cout << "Enter the height of the crucible (h): ";
+        cin >> h;
+        cout << "Enter the initial concentration (C0): ";
+        cin >> C0;
+        cout << "Enter the equilibrium concentration (Cp): ";
+        cin >> Cp;
+        cout << "Enter the initial value (C): ";
+        cin >> Cstart;
+        cout << "Enter the final value (C): ";
+        cin >> Cend;
+        
+        double start = min(Cstart, Cend);
+        double end = max(Cstart, Cend);
+        double direction = (Cstart <= Cend) ? step : -step;
+        
+        for (double C = Cstart; 
+            (direction > 0 && C <= Cend) || (direction < 0 && C >= Cend); 
+            C += direction) {
+            
+            double t = sqrt(2.0) * (C0 - Cp) / (M_PI * (C0 - C));
+            if (t > 2) {
+                iResult = sqrt(2.0) * D * t / (h * h) * (C0 - Cp);
+            } else {
+                iResult = 0.25 * D * M_PI * M_PI / (h * h) * (C0 - Cp);
+            }
+            
+            cout << " t = " << t << " i = " << iResult << endl;        
         }
-
-        double t = sqrt(2.0) * (C0 - Cp) / (M_PI * (C0 - C));
-
-        if (t > 2) {
-            i_result = sqrt(2) * D * t / (h * h) * (C0 - Cp);
-        } else {
-            i_result = 0.25 * D * M_PI * M_PI / (h * h) * (C0 - Cp);
-        }
-
-        cout << "t = " << t << ", i = " << i_result << endl;
+        
+        return 0;
     }
-
-    return 0;
-}
